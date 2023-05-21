77deb68e69e633844e55475ff7c496f4907ee973
Tiago Cunha <tiago92cunha@gmail.com>
2023-05-21 18:57:46 +0100

    Add bookmark icon

--- src/components/ui/Thumbnail.tsx
+++ src/components/ui/Thumbnail.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react"
 import Image from "next/image"
 
+import { Bookmark } from "../svg/Bookmark"
 import { ItemTitle } from "./ItemTitle"
 
 type Props = {
@@ -36,6 +37,9 @@ export const Thumbnail: React.FC<Props> = ({
         layout="fill"
         className="rounded-lg"
       />
+      <div className="absolute top-2 right-2 bg-gradient-to-r from-[#00000080] to-[#00000080] z-20 h-8 w-8 rounded-full flex justify-center items-center hover:cursor-pointer hover:fill-white fill-none">
+        <Bookmark className="fill-inherit" />
+      </div>
       {isTrending && (
         <div className="absolute bottom-6 left-6 flex flex-col gap-1">
           <ItemTitle type={type!} year={year} rating={rating} title={title} />
